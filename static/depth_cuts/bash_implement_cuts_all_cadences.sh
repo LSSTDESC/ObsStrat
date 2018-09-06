f3ce57345d7e0b171d65c6179177147228d28f0e
Humna <humna.awan@rutgers.edu>
2018-09-05 18:34:38 -0700

    Updated script for depth/ebv cuts for the 4 new cadences.

--- static/depth_cuts/bash_implement_cuts_all_cadences.sh
+++ static/depth_cuts/bash_implement_cuts_all_cadences.sh
@@ -7,8 +7,9 @@ setup sims_maf -r /global/homes/a/awan/LSST/lsstRepos/sims_maf
 
 current_dir=$(pwd)
 # run the script for all the cadences
-for db in baseline2018a kraken_2026 kraken_2035 kraken_2036 colossus_2665 \
-          colossus_2664 colossus_2667 pontus_2002 pontus_2489 pontus_2502 mothra_2045
+#for db in baseline2018a kraken_2026 kraken_2035 kraken_2036 colossus_2665 \
+#          colossus_2664 colossus_2667 pontus_2002 pontus_2489 pontus_2502 mothra_2045
+for db in kraken_2042 kraken_2044 mothra_2049 nexus_2097
 do
     echo Working on $db
     python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/static/depth_cuts/implement_depth_ebv_cuts.py  \
@@ -25,4 +26,4 @@ done
 cd /global/homes/a/awan/desc/
 chgrp -R lsst depth_data_outputs
 chmod -R g-w depth_data_outputs
-cd current_dir
+cd $current_dir
