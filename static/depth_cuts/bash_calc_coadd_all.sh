a316f6c8c986048604e92cd370f44bc346d60142
Humna <humna.awan@rutgers.edu>
2018-09-05 18:33:43 -0700

    Updated the coadd script to include the four new cadences.

--- static/depth_cuts/bash_calc_coadd_all.sh
+++ static/depth_cuts/bash_calc_coadd_all.sh
@@ -7,8 +7,19 @@ setup sims_maf -r /global/homes/a/awan/LSST/lsstRepos/sims_maf
 setup sims_maf_contrib -r /global/homes/a/awan/LSST/lsstRepos/sims_maf_contrib
 
 # run the script for all the cadences
+#for yr_cut in 1 3 6 10
+#do
+#    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/calc_coadd_depth.py --nside=256 --yr_cut=$yr_cut \
+#                                                            --outDir='/global/homes/a/awan/LSST/output/coadd_output_allwps_perNight/' &
+#done
+
+# run the script for the new cadences
 for yr_cut in 1 3 6 10
 do
-    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/calc_coadd_depth.py --nside=256 --yr_cut=$yr_cut \
+    for db in kraken_2042.db kraken_2044.db mothra_2049.db nexus_2097.db
+    do
+        python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/calc_coadd_depth.py --nside=256 --yr_cut=$yr_cut \
+                                                            --specific_db=$db \
                                                             --outDir='/global/homes/a/awan/LSST/output/coadd_output_allwps_perNight/' &
+    done
 done
