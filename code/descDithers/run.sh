4302111273d4fbfdc1e4149effd50f91d6d5b07c
Humna <humna.awan@rutgers.edu>
2018-09-05 21:45:26 -0700

    Updated bash script. Removed unused files.

--- code/descDithers/run.sh
+++ code/descDithers/run.sh
@@ -6,4 +6,8 @@ source /global/common/software/lsst/cori-haswell-gcc/stack/setup_current_sims.sh
 setup sims_maf -r /global/homes/a/awan/LSST/lsstRepos/sims_maf
 
 # run the script
-python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/descDithers/descDiths_wp_cadences.py
+python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/descDithers/save_csv_dithers.py \
+                            --dbs_path='/global/cscratch1/sd/awan/dbs_wp_unzipped' \
+                            --outDir='/global/homes/a/awan/desc/wp_descDithers_csvs' \
+                            --rot_rand_seed=42 --trans_rand_seed=42 --save_plots
+                            #--db_files_only='baseline2018a.db'
