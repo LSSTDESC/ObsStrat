2fd9ce0f504b052e4adba1c650cfaf688ab0ea02
Humna <humna.awan@rutgers.edu>
2018-10-11 19:16:36 -0700

    Changed plotting script name to be more general + updated usage.

--- code/modifyWFD/seeing/bash_plot_seeing.sh
+++ code/modifyWFD/seeing/bash_plot_seeing.sh
@@ -8,14 +8,14 @@ setup sims_maf -r /global/homes/a/awan/LSST/lsstRepos/sims_maf
 ####################################################################################################
 # run the script for seeing
 # OpSim seeing
-python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_seeing_maps.py \
+python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
                                 --db_path='/global/cscratch1/sd/awan/dbs_wp_unzipped' \
                                 --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
                                 --baseline_and_wide_only --nside=256 --quantity='seeingFwhmEff' &
 # Eric N.'s outp#uts
 for tag  in ow6 ow6c ow7 ow7c
 do
-    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_seeing_maps.py \
+    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
                                 --db_path=/global/cscratch1/sd/awan/owsee_dbs/${tag} \
                                 --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
                                 --baseline_and_wide_only --nside=256 --quantity='seeingFwhmEff' &
