c78e13900c7112de1b3fe0e5bd431a9e706140f3
Humna <humna.awan@rutgers.edu>
2018-10-11 19:17:47 -0700

    Updated bash script to plot skymaps for seeing and fiveSigmaDepth.

---
+++ code/modifyWFD/seeing/bash_plot_seeing_fiveSigmaDepth.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# source sims_maf
+source /global/common/software/lsst/cori-haswell-gcc/stack/setup_current_sims.sh
+# set up my own version rn
+setup sims_maf -r /global/homes/a/awan/LSST/lsstRepos/sims_maf
+
+####################################################################################################
+# run the script for seeing
+# OpSim seeing
+python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
+                                --db_path='/global/cscratch1/sd/awan/dbs_wp_unzipped' \
+                                --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
+                                --baseline_and_wide_only --nside=256 --quantity='seeingFwhmEff'
+
+# Eric N.'s outputs
+for tag in ow6 ow6c ow7 ow7c
+do
+    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
+                                --db_path=/global/cscratch1/sd/awan/owsee_dbs/${tag} \
+                                --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
+                                --baseline_and_wide_only --nside=256 --quantity='seeingFwhmEff'
+done
+
+
+####################################################################################################
+# run the script for five sigma depth
+# OpSim seeing
+python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
+                                --db_path='/global/cscratch1/sd/awan/dbs_wp_unzipped' \
+                                --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
+                                --baseline_and_wide_only --nside=256 --quantity='fiveSigmaDepth'
+
+# Eric N.'s outputs
+for tag  in ow6 ow6c ow7 ow7c
+do
+    python /global/homes/a/awan/LSST/lsstRepos/ObsStrat/code/modifyWFD/seeing/plot_mean_maps.py \
+                                --db_path=/global/cscratch1/sd/awan/owsee_dbs/${tag} \
+                                --outDir='/global/cscratch1/sd/awan/lsst_output/owsee_comparisons/' \
+                                --baseline_and_wide_only --nside=256 --quantity='fiveSigmaDepth'
+done
