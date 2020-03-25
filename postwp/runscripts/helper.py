0d92d03eac52f41365c416eaf247c9f76cf493c4
Humna <humna.awan@rutgers.edu>
2020-03-24 21:55:15 -0700

    dealt with new v1.4 dbs folder; updated folder-map.

--- postwp/runscripts/helper.py
+++ postwp/runscripts/helper.py
@@ -1,136 +1,79 @@
 # dictionary mapping the overall categories of the new fbs outputs and the db filenames to look for for each group.
-folder_map = {
-'filter_changer':
-    [
-        'fc1exp_pairsmix_ilim15_10yrs.db',
-        'fc1exp_pairsmix_ilim60_10yrs.db',
-        'fc1exp_pairsmix_ilim30_10yrs.db'
-    ],
-'footprints':
-    [
-        'baseline10yrs.db',
-        'newA10yrs.db',
-        'big_sky_nouiy10yrs.db',
-        'big_sky10yrs.db',
-        'stuck_rolling10yrs.db',
-        'newB10yrs.db',
-        'bluer_footprint10yrs.db',
-        'gp_heavy10yrs.db',
-        'big_sky_dust_v1.3_10yrs.db'
-    ],
-'alt_sched':
-    [
-        'very_alt10yrs.db',
-        'very_alt2_rm5illum20_10yrs.db',
-        'very_alt_rm510yrs.db',
-        'very_alt3_rm5illum40_10yrs.db',
-        'very_alt3_rm5illum25_10yrs.db',
-        'dec_1exp_pairsmix_10yrs.db',
-        'very_alt2_rm5illum40_10yrs.db',
-        'very_alt3_rm5illum20_10yrs.db',
-        'very_alt2_rm5illum15_10yrs.db',
-        'very_alt2_rm5illum50_10yrs.db',
-        'very_alt2_rm5illum25_10yrs.db',
-        'very_alt3_rm5illum50_10yrs.db',
-        'very_alt3_rm5illum15_10yrs.db',
-        'very_alt2_rm5illum30_10yrs.db',
-        'very_alt3_rm5illum30_10yrs.db',
-        'altwfd_v1.3_10yrs.db',
-        'altroll_mod2_sdf_0.20_v1.3_10yrs.db'
-    ],
-'exp_time':
-     [
-         'exptime_1exp_pairsmix_10yrs.db'
-     ],
-'baselines':
-     [
-        'baseline_1exp_nopairs_10yrs.db',
-        'baseline_1exp_pairsmix_10yrs.db',
-        'baseline_2exp_pairsmix_10yrs.db',
-        'baseline_2exp_pairsame_10yrs.db',
-        'baseline_1exp_pairsame_10yrs.db',
-        'noddf_1exp_pairsame_10yrs.db'
-     ],
-'rolling_cadence':
-    [
-        'roll_mod6_sdf0.20mixed_10yrs.db',
-        'roll_mod6_sdf0.05mixed_10yrs.db',
-        'simple_roll_mod10_sdf0.20mixed_10yrs.db',
-        'roll_mod3_sdf0.20mixed_10yrs.db',
-        'roll_mod2_sdf0.20mixed_10yrs.db',
-        'roll_mod6_sdf0.10mixed_10yrs.db',
-        'roll_mod3_sdf0.05mixed_10yrs.db',
-        'simple_roll_mod2_sdf0.20mixed_10yrs.db',
-        'roll_mod2_sdf0.05mixed_10yrs.db',
-        'roll_mod2_sdf0.10mixed_10yrs.db',
-        'simple_roll_mod5_sdf0.20mixed_10yrs.db',
-        'roll_mod3_sdf0.10mixed_10yrs.db',
-        'simple_roll_mod3_sdf0.20mixed_10yrs.db'
-    ],
-'ToO':
-     [
-        'too_pairsmix_rate100_10yrs.db',
-        'too_pairsmix_rate50_10yrs.db',
-        'too_pairsmix_rate1_10yrs.db',
-        'too_pairsmix_rate10_10yrs.db'
-     ],
-'weather':
-     [
-        'weather_0.60c_10yrs.db',
-        'weather_0.40c_10yrs.db',
-        'weather_0.20c_10yrs.db',
-        'weather_0.10c_10yrs.db',
-        'weather_0.90c_10yrs.db',
-        'weather_0.30c_10yrs.db',
-        'weather_1.10c_10yrs.db',
-        'weather_0.70c_10yrs.db',
-        'weather_0.80c_10yrs.db'
-     ],
-'short_expt':
-    [
-        'shortt_5ns_5ext_pairsmix_10yrs.db',
-        'shortt_5ns_1ext_pairsmix_10yrs.db',
-        'shortt_2ns_1ext_pairsmix_10yrs.db',
-        'shortt_2ns_5ext_pairsmix_10yrs.db'
-    ],
-'template_gen':
-    [
-        'templates_w_2.0_1exp_pairsmix_10yrs.db',
-        'templates_w_3.0_1exp_pairsmix_10yrs.db',
-        'templates_w_4.0_1exp_pairsmix_10yrs.db',
-        'templates_w_1.0_1exp_pairsmix_10yrs.db',
-        'templates_w_5.0_1exp_pairsmix_10yrs.db'
-    ],
-'rotator':
-    [
-        'rotator_1exp_pairsmix_10yrs.db'
-    ],
-'presto':
-    [
-        'presto_third_10yrs.db',
-        'presto_10yrs.db'
-    ],
-'stability_test':
-    [
-        'stability_1offset_43seed10yrs.db',
-        'stability_365offset_42seed10yrs.db',
-        'stability_30offset_42seed10yrs.db',
-        'stability_10offset_42seed10yrs.db',
-        'stability_180offset_42seed10yrs.db',
-        'stability_-10offset_42seed10yrs.db',
-        'stability_1offset_44seed10yrs.db',
-        'stability_1offset_42seed10yrs.db'
-    ],
-'DDF':
-    [
-        'ddf_0.23deg_1exp_pairsmix_10yrs.db',
-        'ddf_pn_0.23deg_1exp_pairsmix_10yrs.db',
-        'desc_ddf_pn_0.70deg_1exp_pairsmix_10yrs.db',
-        'ddf_0.70deg_1exp_pairsmix_10yrs.db',
-        'ddf_pn_0.70deg_1exp_pairsmix_10yrs.db'
-    ]
-}
+folder_map = {'alt_roll_dust': ['alt_roll_mod2_dust_sdf_0.20_v1.4_10yrs.db',
+  'roll_mod2_dust_sdf_0.20_v1.4_10yrs.db'],
+ 'rolling': ['rolling_mod3_sdf_0.10_v1.4_10yrs.db',
+  'rolling_mod6_sdf_0.20_v1.4_10yrs.db',
+  'rolling_mod2_sdf_0.20_v1.4_10yrs.db',
+  'rolling_mod2_sdf_0.10_v1.4_10yrs.db',
+  'rolling_mod3_sdf_0.20_v1.4_10yrs.db',
+  'rolling_mod6_sdf_0.10_v1.4_10yrs.db'],
+ 'footprints': ['footprint_bluer_footprintv1.4_10yrs.db',
+  'footprint_no_gp_northv1.4_10yrs.db',
+  'footprint_big_skyv1.4_10yrs.db',
+  'footprint_big_sky_dustv1.4_10yrs.db',
+  'footprint_standard_goalsv1.4_10yrs.db',
+  'footprint_newBv1.4_10yrs.db',
+  'footprint_big_sky_nouiyv1.4_10yrs.db',
+  'footprint_gp_smoothv1.4_10yrs.db',
+  'footprint_newAv1.4_10yrs.db',
+  'footprint_stuck_rollingv1.4_10yrs.db',
+  'footprint_add_mag_cloudsv1.4_10yrs.db'],
+ 'var_expt': ['var_expt_v1.4_10yrs.db'],
+ 'twilight_neo': ['twilight_neo_mod4_v1.4_10yrs.db',
+  'twilight_neo_mod3_v1.4_10yrs.db',
+  'twilight_neo_mod2_v1.4_10yrs.db',
+  'twilight_neo_mod1_v1.4_10yrs.db'],
+ 'DCR': ['dcr_nham4_v1.4_10yrs.db',
+  'dcr_nham2_v1.4_10yrs.db',
+  'dcr_nham3_v1.4_10yrs.db',
+  'dcr_nham1_v1.4_10yrs.db',
+  'dcr_nham5_v1.4_10yrs.db'],
+ 'bulge': ['bulges_i_heavy_v1.4_10yrs.db',
+  'bulges_cadence_i_heavy_v1.4_10yrs.db',
+  'bulges_bs_v1.4_10yrs.db',
+  'bulges_cadence_bs_v1.4_10yrs.db',
+  'bulges_cadence_bulge_wfd_v1.4_10yrs.db',
+  'bulges_bulge_wfd_v1.4_10yrs.db'],
+ 'AGN_DDF': ['agnddf_v1.4_10yrs.db'],
+ 'weather': ['weather_1.2_v1.4_10yrs.db',
+  'weather_0.7_v1.4_10yrs.db',
+  'weather_0.3_v1.4_10yrs.db',
+  'weather_1.2_ndt_v1.4_10yrs.db'],
+ 'pair_strat': ['pair_strategy_0_v1.4_10yrs.db',
+  'pair_strategy_1_v1.4_10yrs.db',
+  'pair_strategy_4_v1.4_10yrs.db',
+  'pair_strategy_2_v1.4_10yrs.db',
+  'pair_strategy_3_v1.4_10yrs.db'],
+ 'wfd_depth': ['wfd_depth_scale0.95_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.80_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.75_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.99_v1.4_10yrs.db',
+  'wfd_depth_scale0.70_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.99_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.65_v1.4_10yrs.db',
+  'wfd_depth_scale0.90_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.95_v1.4_10yrs.db',
+  'wfd_depth_scale0.75_v1.4_10yrs.db',
+  'wfd_depth_scale0.90_v1.4_10yrs.db',
+  'wfd_depth_scale0.80_v1.4_10yrs.db',
+  'wfd_depth_scale0.70_v1.4_10yrs.db',
+  'wfd_depth_scale0.65_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.85_noddf_v1.4_10yrs.db',
+  'wfd_depth_scale0.85_v1.4_10yrs.db'],
+ 'twilight_filters': ['twi_filters_3_v1.4_10yrs.db',
+  'twi_filters_1_v1.4_10yrs.db',
+  'twi_filters_2_v1.4_10yrs.db',
+  'twi_filters_4_v1.4_10yrs.db',
+  'twi_filters_5_v1.4_10yrs.db'],
+ 'baseline': ['baseline_2snapsv1.4_10yrs.db', 'baseline_v1.4_10yrs.db'],
+ 'short_exp': ['short_exp_2ns_1expt_v1.4_10yrs.db',
+  'short_exp_2ns_5expt_v1.4_10yrs.db',
+  'short_exp_5ns_1expt_v1.4_10yrs.db',
+  'short_exp_5ns_5expt_v1.4_10yrs.db'],
+ 'DESC_DDF': ['descddf_v1.4_10yrs.db'],
+ 'spiders': ['spiders_v1.4_10yrs.db'],
+ 'euclid_DDF': ['euclidddf_v1.4_10yrs.db']}
+
 for group in folder_map:
     for i, key in enumerate( folder_map[group] ):
         folder_map[group][i] = key.split('.db')[0]
-#folder_map
